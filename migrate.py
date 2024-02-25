aa721db8bf8edac8bd180a3b8d2e5ec37c2ff338
mikeill <mike@mzoo.org>
2024-02-25 08:09:35 -0600

    Grab title.

--- migrate.py
+++ migrate.py
@@ -6,10 +6,13 @@ import re
 
 location = pathlib.Path().resolve()
 for f in os.listdir('older'):
+  print('Details for ' + f)
   try:
     with open (str(location) + '/older/' + f, 'r', encoding="UTF-8") as old:
       for line in old:
-        m = re.search(r"(\d{1,2})/(\d{1,2})/(\d{2,4})", line)
-        if m: print(m.group(1) + '/' + m.group(2)  + '/' + m.group(3))
+        date = re.search(r"(\d{1,2})/(\d{1,2})/(\d{2,4})", line)
+        if date: print(date.group(1) + '/' + date.group(2)  + '/' + date.group(3))
+        title = re.search(r"<TITLE>(.*)</TITLE>", line)
+        if title: print(title.group(1))
   except UnicodeDecodeError:
     print(f + " is not UTF-8")
