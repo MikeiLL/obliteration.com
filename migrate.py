c1725edd8d803dc84bdf668c2e0db958f3dcc41a
mikeill <mike@mzoo.org>
2024-02-25 08:05:25 -0600

    Extract file dates.

--- migrate.py
+++ migrate.py
@@ -1,10 +1,15 @@
 import os
 import sys
 import pathlib
+import re
 
 
 location = pathlib.Path().resolve()
 for f in os.listdir('older'):
-  with open (str(location) + '/older/' + f, 'r') as old:
-    for line in old:
-      print(line)
+  try:
+    with open (str(location) + '/older/' + f, 'r', encoding="UTF-8") as old:
+      for line in old:
+        m = re.search(r"(\d{1,2})/(\d{1,2})/(\d{2,4})", line)
+        if m: print(m.group(1) + '/' + m.group(2)  + '/' + m.group(3))
+  except UnicodeDecodeError:
+    print(f + " is not UTF-8")
