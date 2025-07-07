b1a7c2d9ec8a36f7263b3d7089a15a17462a6f64
mikeill <mike@mzoo.org>
2025-07-06 20:42:03 -0500

    Go back to my less elegant, but working, approach.

--- reopen.sh
+++ reopen.sh
@@ -1,1 +1,3 @@
 cd _posts && ls -Art | tail -n 1 | xargs open -a Visual\ Studio\ Code.app && cd ../
+# Rosuav recommend using backtic: SciTE `ls stillebot/* -t|head -n 1`
+#not currently working though.
