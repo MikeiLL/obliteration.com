4af4efad610a8dbfffa81ca9095d692f31650be8
mikeill <mike@mzoo.org>
2025-07-07 09:30:20 -0500

    Rosuav more elegant Backtick approach working.

--- reopen.sh
+++ reopen.sh
@@ -1,3 +1,3 @@
-cd _posts && ls -Art | tail -n 1 | xargs open -a Visual\ Studio\ Code.app && cd ../
-# Rosuav recommend using backtic: SciTE `ls stillebot/* -t|head -n 1`
-#not currently working though.
+code `ls _posts/* -t|tail -n 1`
+# If I wanted to alias this I'd have to replace "code"
+# with open -a Visual\ Studio\ Code.app
